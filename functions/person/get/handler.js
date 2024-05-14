450a929478fc206240bd5f8d1c2b03428e0cedea
Evert Ortiz <evert.ortiz.m@gmail.com>
2024-05-14 10:48:42 -0500

    chore: code refactor

--- functions/person/get/handler.js
+++ functions/person/get/handler.js
@@ -1,18 +1,7 @@
-const crypto = require('crypto');
 const aws = require('aws-sdk');
 const { formatResponse } = require('../../../utils/formatResponse');
+const { getPersonById } = require('../../../utils/personService');
 
-
-const isTest = process.env.JEST_WORKER_ID;
-const config = {
-  convertEmptyValues: true,
-  ...(isTest && {
-    endpoint: 'http://localhost:8000',
-    sslEnabled: false,
-    region: 'local-env',
-  }),
-};
-const dynamodb = new aws.DynamoDB.DocumentClient(config);
 
 const get = async (event) => {
   let personId = event.pathParameters.id;
@@ -23,7 +12,7 @@ const get = async (event) => {
     },
   };
 
-  const { Item: item } = await dynamodb.get(params).promise();
+  const item = await getPersonById(personId);
   if (!item) return formatResponse(403, { error: "Person not found" });
   return formatResponse(200, item);
 
